8266405dcb4727aca49ec50d3341ea1a209dd89b
Santiago <122063233+SaviNPC@users.noreply.github.com>
2023-02-04 03:14:58 -0300

    Semantic HTML. Meta tagsstatus

---
+++ src/Footer.js
@@ -0,0 +1,28 @@
+
+function Footer(){
+    return (
+    <footer>
+        <img/>
+        <ul className="Navigation"> Navigation
+            <li><a href="home">Home</a></li>
+            <li><a href="about">About</a></li>
+            <li><a href="menu">Menu</a></li>
+            <li><a href="reservations">Reservations</a></li>
+            <li><a href="order">Order Online</a></li>
+            <li><a href="login">Login</a></li>
+        </ul>
+        <ul className="Contact"> Contact
+            <li><a href="address">Address</a></li>
+            <li><a href="phone">Phone Number</a></li>
+            <li><a href="email">email</a></li>
+        </ul>
+        <ul className="Social Media links"> Social Media
+            <li><a href="instagram">Instagram</a></li>
+            <li><a href="facebook">Facebook</a></li>
+            <li><a href="twitter">Twitter</a></li>
+        </ul>
+    </footer>
+    )
+};
+
+export default Footer;
