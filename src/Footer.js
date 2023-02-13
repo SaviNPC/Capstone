17f93793a419339698bc86c9225fde8a202d9c64
Santiago <122063233+SaviNPC@users.noreply.github.com>
2023-02-13 06:02:12 -0300

    Responsive design

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import './App.css';
 function Footer(){
     return (
     <footer>
-        <img src={require('./Logo.png')}/>
+        <a href='index.html'><img className="logo" src={require('./Logo.png')} alt="logo"/></a>
         <ul className="Navigation"> Navigation
             <li><a href="home">Home</a></li>
             <li><a href="about">About</a></li>
@@ -19,9 +19,9 @@ function Footer(){
             <li><a href="email">email</a></li>
         </ul>
         <ul className="Social Media links"> Social Media
-            <li><a href="instagram">Instagram</a></li>
-            <li><a href="facebook">Facebook</a></li>
-            <li><a href="twitter">Twitter</a></li>
+            <li><a href="instagram" className="ig">Instagram</a></li>
+            <li><a href="facebook" className="fb">Facebook</a></li>
+            <li><a href="twitter" className="tw">Twitter</a></li>
         </ul>
     </footer>
     )
--- src/Main.js
+++ src/Main.js
@@ -74,45 +74,45 @@ function Main(){
         <section className='testimonials'>
             <h1>Testimonials</h1>
             <div className="testimonies">
-                <article className='rate1'>
-                    <h1>Rating</h1>
-                    <img src={require("./images/eye.png")} alt="Eye icon" style={{
-                        width: 30,
-                        height: 20,
-                        borderRadius: 10,
+                <article className='rate'>
+                    <h2>10/10</h2>
+                    <img src={require("./images/olivia.png")} alt="Profile icon" style={{
+                        width: 100,
+                        height: 100,
+                        borderRadius: 50,
                     }}/>
-                    <p>Name</p>
-                    <p2>Review Text</p2>
+                    <p>Olivia Smith</p>
+                    <p2>My experience at the Little Lemon restaurant can be summarized as an an excellent combination of flavors and aromas, culminating in a perfect dish </p2>
                 </article>
-                <article className='rate2'>
-                    <h1>Rating</h1>
-                    <img src={require("./images/eye.png")} alt="Eye icon" style={{
-                        width: 30,
-                        height: 20,
-                        borderRadius: 10,
+                <article className='rate'>
+                    <h2>9.5/10</h2>
+                    <img src={require("./images/sam.png")} alt="Profile icon" style={{
+                        width: 100,
+                        height: 100,
+                        borderRadius: 50,
                     }}/>
-                    <p>Name</p>
-                    <p2>Review Text</p2>
+                    <p>Sam Perez</p>
+                    <p2>I loved the Bruschetta! Will be back to try the Lemon Dessert</p2>
                 </article>
-                <article className='rate3'>
-                    <h1>Rating</h1>
-                    <img src={require("./images/eye.png")} alt="Eye icon" style={{
-                        width: 30,
-                        height: 20,
-                        borderRadius: 10,
+                <article className='rate'>
+                    <h2>9.8/10</h2>
+                    <img src={require("./images/jhon.png")} alt="Profile icon" style={{
+                        width: 100,
+                        height: 100,
+                        borderRadius: 50,
                     }}/>
-                    <p>Name</p>
-                    <p2>Review Text</p2>
+                    <p>John Doe</p>
+                    <p2>A red wine together with a tasty seasoned pasta made the perfect combination for a beautiful date night with my partner.</p2>
                 </article>
-                <article className='rate4'>
-                    <h1>Rating</h1>
-                    <img src={require("./images/eye.png")} alt="Eye icon" style={{
-                        width: 30,
-                        height: 20,
-                        borderRadius: 10,
+                <article className='rate'>
+                    <h2>10/10</h2>
+                    <img src={require("./images/claudia.png")} alt="Profile icon" style={{
+                        width: 100,
+                        height: 100,
+                        borderRadius: 50,
                     }}/>
-                    <p>Name</p>
-                    <p2>Review Text</p2>
+                    <p>Claudia Jones</p>
+                    <p2>An experience full of flavor and a friendly staff. I spoke directly with the owners and I highlight their warm and human attention explaining the magic behind the dishes.</p2>
                 </article>
 
             </div>
@@ -123,16 +123,18 @@ function Main(){
             <h2>Chicago</h2>
             <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
 Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. </p>
-            <img src={require("./images/chef.jpg")} alt="Chef chooking" style={{
-                width: 300,
-                height: 200,
-                borderRadius: 10,
-            }}/>
-            <img src={require("./images/marioAndAdrian.jpg")} alt="Mario and Adrian" style={{
-                width: 300,
-                height: 200,
-                borderRadius: 10,
-            }}/>
+            <div className="chefPic">
+                <img className="top" src={require("./images/chef.jpg")} alt="Chef chooking" style={{
+                    width: 350,
+                    height: 250,
+                    borderRadius: 10,
+                }}/>
+                <img className="bot" src={require("./images/marioAndAdrian.jpg")} alt="Mario and Adrian" style={{
+                    width: 350,
+                    height: 250,
+                    borderRadius: 10,
+                }}/>
+            </div>
         </secion>
     </main>
     );
