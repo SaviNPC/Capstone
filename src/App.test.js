013fa8e1f2b9aa99c95787e212d347aff72315d7
Santiago <122063233+SaviNPC@users.noreply.github.com>
2023-02-19 03:14:53 -0300

    Final tests. Ux Reviewed

--- src/App.test.js
+++ src/App.test.js
@@ -30,3 +30,102 @@ describe('updateTimes function', () => {
   });
 });
 
+test("HTML5 validation for date input", () => {
+  const { getByLabelText } = render(<Reservations />);
+
+  // Check if the field is required
+  expect(getByLabelText(/Choose date/)).toBeRequired();
+
+  // Test for invalid input - date in the past
+  fireEvent.change(getByLabelText(/Choose date/), { target: { value: "2022-02-15" } });
+  expect(getByLabelText(/Choose date/)).not.toBeValid();
+  expect(getByLabelText(/Choose date/).validationMessage).toBe("Please enter a date in the future.");
+
+  // Test for valid input
+  fireEvent.change(getByLabelText(/Choose date/), { target: { value: "2023-02-25" } });
+  expect(getByLabelText(/Choose date/)).toBeValid();
+});
+
+test("HTML5 validation for time select", () => {
+  const { getByLabelText } = render(<Reservations availableTimes={["10:00 AM", "12:00 PM", "02:00 PM"]} />);
+
+  // Check if the field is required
+  expect(getByLabelText(/Choose time/)).toBeRequired();
+
+  // Test for invalid input - no option selected
+  fireEvent.change(getByLabelText(/Choose time/), { target: { value: "" } });
+  expect(getByLabelText(/Choose time/)).not.toBeValid();
+  expect(getByLabelText(/Choose time/).validationMessage).toBe("Please select an option.");
+
+  // Test for valid input
+  fireEvent.change(getByLabelText(/Choose time/), { target: { value: "12:00 PM" } });
+  expect(getByLabelText(/Choose time/)).toBeValid();
+});
+
+test("HTML5 validation for guests input", () => {
+  const { getByLabelText } = render(<Reservations />);
+
+  // Check if the field is required
+  expect(getByLabelText(/Number of guests/)).toBeRequired();
+
+  // Test for invalid input - value less than min
+  fireEvent.change(getByLabelText(/Number of guests/), { target: { value: "0" } });
+  expect(getByLabelText(/Number of guests/)).not.toBeValid();
+  expect(getByLabelText(/Number of guests/).validationMessage).toBe("Please enter a number that is 1 or greater.");
+
+  // Test for invalid input - value greater than max
+  fireEvent.change(getByLabelText(/Number of guests/), { target: { value: "11" } });
+  expect(getByLabelText(/Number of guests/)).not.toBeValid();
+  expect(getByLabelText(/Number of guests/).validationMessage).toBe("Please enter a number that is 10 or less.");
+
+  // Test for valid input
+  fireEvent.change(getByLabelText(/Number of guests/), { target: { value: "5" } });
+  expect(getByLabelText(/Number of guests/)).toBeValid();
+});
+
+describe("handleGuestsChange", () => {
+  it("should set guests to the parsed integer value of the input", () => {
+    const event = { target: { value: "3" } };
+    handleGuestsChange(event);
+    expect(guests).toEqual(3);
+  });
+});
+
+describe("handleOccasionChange", () => {
+  it("should set occasion to the input value", () => {
+    const event = { target: { value: "Anniversary" } };
+    handleOccasionChange(event);
+    expect(occasion).toEqual("Anniversary");
+  });
+
+  it("should set occasionValid to true if the input value is not empty", () => {
+    const event = { target: { value: "Birthday" } };
+    handleOccasionChange(event);
+    expect(occasionValid).toBe(true);
+  });
+
+  it("should set occasionValid to false if the input value is empty", () => {
+    const event = { target: { value: "" } };
+    handleOccasionChange(event);
+    expect(occasionValid).toBe(false);
+  });
+});
+
+describe("handleSubmit", () => {
+  it("should prevent the default form submission behavior", () => {
+    const event = { preventDefault: jest.fn() };
+    handleSubmit(event);
+    expect(event.preventDefault).toHaveBeenCalled();
+  });
+
+  it("should call setBooking with the correct booking object", () => {
+    const event = { preventDefault: jest.fn() };
+    const booking = { date: "2022-03-21", time: "13:00", guests: 4, occasion: "Birthday" };
+    setDate("2022-03-21");
+    setTime("13:00");
+    setGuests(4);
+    setOccasion("Birthday");
+    handleSubmit(event);
+    expect(setBooking).toHaveBeenCalledWith(booking);
+  });
+});
--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -1,7 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Reservations.css";
-import { useState } from 'react';
-// Date, Time, Number of Guests, Occasion (Birthday, anniversary), Submit reservation button
 
 function Reservations(props) {
 
@@ -11,9 +9,19 @@ function Reservations(props) {
     const [occasion, setOccasion] = useState('Birthday');
     const [date, setDate] = useState("");
 
+    const [dateValid, setDateValid] = useState(false);
+    const [timeValid, setTimeValid] = useState(false);
+    const [occasionValid, setOccasionValid] = useState(false);
+
+    useEffect(() => {
+        const formValid = dateValid && timeValid && occasionValid;
+        const submitButton = document.getElementById("submit-button");
+        submitButton.disabled = !formValid;
+    }, [dateValid, timeValid, occasionValid]);
 
     const handleTimeChange = (event) => {
         setTime(event.target.value);
+        setTimeValid(event.target.value !== "");
     };
 
     const handleGuestsChange = (event) => {
@@ -22,10 +30,12 @@ function Reservations(props) {
 
     const handleOccasionChange = (event) => {
         setOccasion(event.target.value);
+        setOccasionValid(event.target.value !== "");
     };
 
     const handleDateInputChange = (event) => {
         setDate(event.target.value);
+        setDateValid(event.target.value !== "");
         handleDateChange(event.target.value);
     };
 
@@ -38,24 +48,27 @@ function Reservations(props) {
     return (
         <form onSubmit={handleSubmit}>
             <label htmlFor={"res-date"}>Choose date</label>
-            <input type="date" id="res-date" value={date} onChange={handleDateInputChange} />
-            
+            <input type="date" id="res-date" value={date} onChange={handleDateInputChange} required/>
+
             <label htmlFor="res-time">Choose time</label>
-            <select id="res-time" value={time} onChange={handleTimeChange}>
+            <select id="res-time" value={time} onChange={handleTimeChange} required>
+                <option value="">Select a time</option>
                 {availableTimes.map((time) => (
-                    <option key={time}>{time}</option>
+                <option key={time} value={time}>{time}</option>
                 ))}
             </select>
-            
+
             <label htmlFor="guests">Number of guests</label>
-            <input value={guests} onChange={handleGuestsChange} type="number" placeholder="1" min="1" max="10" id="guests" />
-            
+            <input value={guests} onChange={handleGuestsChange} type="number" placeholder="1" min="1" max="10" id="guests" required/>
+
             <label htmlFor="occasion">Occasion</label>
-            <select id="occasion" value={occasion} onChange={handleOccasionChange}>
-                <option>Birthday</option>
-                <option>Anniversary</option>
+            <select id="occasion" value={occasion} onChange={handleOccasionChange} required>
+                <option value="">Select an occasion</option>
+                <option value="Birthday">Birthday</option>
+                <option value="Anniversary">Anniversary</option>
             </select>
-            <input type="submit" value="Make Your reservation" />
+
+            <input type="submit" value="Make Your reservation" id="submit-button" disabled />
         </form>
     )
 };
